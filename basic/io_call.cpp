8adf909044858f7346fee8c024a4bd9b10486854
fly-lynx <21mt0432@gmail.com>
2024-11-02 07:19:49 +0000

    Adding io_call.cpp

---
+++ basic/io_call.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int a,b;
+    int mul;
+    cout<<"Enter values for multiplication: " << endl;
+    cout<<"Enter first value:"<<endl;
+    cin>>a;                                                //Taking input from user
+    cout<<"Enter second value"<<endl;
+    cin>>b;                                                // Taking input from user
+    mul = a*b;
+    cout<<"result: "<< mul<<endl;
+
+    return 0;
+}
